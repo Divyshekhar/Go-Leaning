d9534a36fdb80285469fc877103667d32809f505
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-05-18 18:02:11 +0530

    add: range

---
+++ 11_range/range.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	//range is used for iterating over datastructures
+
+	nums := []int{6, 7, 8}
+	sum := 0
+	// for index, num := range nums{}
+	for _, num := range nums {
+		fmt.Println(num)
+		sum += num
+		fmt.Println("sum = ", sum)
+
+	}
+	m := map[string]string{"fname": "john", "lname": "does"}
+
+	for key, val := range m {
+		fmt.Println(key, val)
+	}
+	//unicode code point rune
+	//i is starting byte of rune
+	for i, c := range "golang" {
+		fmt.Println(i, c)
+	}
+
+}
